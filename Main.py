48fdca4963182c36197a2d7f46555596f8d2dfea
Stav Cohen <cohenstav1@gmail.com>
2019-08-12 12:30:51 +0300

    sorting the network data

--- Main.py
+++ Main.py
@@ -1,26 +1,40 @@
 import psutil
 from psutil._common import bytes2human
 def pprint_ntuple(nt):
+    details=[]
     for name in nt._fields:
         value = getattr(nt, name)
         if name != 'percent':
             value = bytes2human(value)
-        print('%-10s : %7s' % (name.capitalize(), value))
-
+        details.append(('%-10s : %7s' % (name.capitalize(), value)))
+    return details
 
 
 def main():
 
-   print(psutil.net_io_counters())
-   pprint_ntuple(psutil.net_io_counters())
+   network_details=[]
+   network_details=pprint_ntuple(psutil.net_io_counters())
+   print(network_details)
+   bytes_sent =network_details[0]
+   bytes_recv=network_details[1]
+   packets_sent=network_details[2]
+   packets_recv=network_details[3]
+   i=0
+   while i<4:
+       print(network_details[i])
+       i=i+1
+
+   '''
    print("X" * 100)
    svmem = psutil.virtual_memory()
    pprint_ntuple(svmem)
    print("X" * 100)
+
+
    cpu = psutil.cpu_percent(interval=1, percpu=False)
    print(f"{cpu} Precent")
 
-
+    '''
 
 
 
